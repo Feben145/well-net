d2c788b6f18013c877d42454c0352aba3476a3d6
febenTaye <feben.arsenal@gmail.com>
2026-06-07 02:03:16 +0300

    infrastructure: temporarily purge frontend submodule tracking

--- frontend/packages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .models import PackageListView, MySubscriptionsView
-
-urlpatterns = [
-    path("",     PackageListView.as_view()),
-    path("my/",  MySubscriptionsView.as_view()),
-]
